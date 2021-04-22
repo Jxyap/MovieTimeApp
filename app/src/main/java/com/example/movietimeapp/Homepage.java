1595baa8fca0d11a9231e73abacd5e7252457a7d
jxyap <82267461+Jxyap@users.noreply.github.com>
2021-04-22 15:35:15 +0800

    change of image slider

--- app/src/main/java/com/example/movietimeapp/Homepage.java
+++ app/src/main/java/com/example/movietimeapp/Homepage.java
@@ -2,29 +2,23 @@ package com.example.movietimeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.CompositePageTransformer;
-import androidx.viewpager2.widget.MarginPageTransformer;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Homepage extends AppCompatActivity {
 
     private Button ns_Btn, cs_btn;
-    private ViewPager2 viewPager2;
-    private Handler sliderHandler = new Handler();
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,43 +30,13 @@ public class Homepage extends AppCompatActivity {
 
         ns_Btn = findViewById(R.id.btn_ns);
         cs_btn = findViewById(R.id.btn_cs);
+        viewPager = findViewById(R.id.image_slider);
 
-        viewPager2 = findViewById(R.id.image_slider);
+        imageAdapter imageAdapter = new imageAdapter(this);
+        viewPager.setAdapter(imageAdapter);
 
-        //Here, prepare list of images from drawable
-        //You can get it from API as well
-
-        List<imageSlider> sliderItem = new ArrayList<>();
-        sliderItem.add(new imageSlider(R.drawable.godzilla_vs_kong));
-        sliderItem.add(new imageSlider(R.drawable.tom_and_jerry));
-
-        viewPager2.setAdapter(new ImageAdapter(sliderItem, viewPager2));
-
-        viewPager2.setClipToPadding(false);
-        viewPager2.setClipChildren(false);
-        viewPager2.setOffscreenPageLimit(3);
-        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
-
-        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
-        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
-        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
-            @Override
-            public void transformPage(@NonNull View page, float position) {
-                float r = 1 - Math.abs(position);
-                page.setScaleY(0.85f + r * 0.15f);
-            }
-        });
-
-        viewPager2.setPageTransformer(compositePageTransformer);
-
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                sliderHandler.removeCallbacks(sliderRunnable);
-                sliderHandler.postDelayed(sliderRunnable, 3000); //slide duration 3 seconds
-            }
-        });
+        Timer timer =new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTasks(),2000, 3000);
 
         BottomNavigationView btm_nav = findViewById(R.id.btm_nav_bar);
         btm_nav.setSelectedItemId(R.id.navigation_home);
@@ -100,10 +64,20 @@ public class Homepage extends AppCompatActivity {
         });
     }
 
-    private Runnable sliderRunnable = new Runnable() {
+    public class MyTimerTasks extends TimerTask{
+
         @Override
         public void run() {
-            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+            Homepage.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (viewPager.getCurrentItem()==0){
+                        viewPager.setCurrentItem(1);
+                    }else{
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
         }
-    };
+    }
 }

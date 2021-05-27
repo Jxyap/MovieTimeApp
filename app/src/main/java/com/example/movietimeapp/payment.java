9380c8ebc483b8428b1ebaf2483696a54569a359
ckhuey <choongkithuey@gmail.com>
2021-05-28 04:07:27 +0800

    payment

--- app/src/main/java/com/example/movietimeapp/payment.java
+++ app/src/main/java/com/example/movietimeapp/payment.java
@@ -10,24 +10,19 @@ import android.widget.Spinner;
 
 public class payment extends AppCompatActivity {
 
-    Spinner cardtype;
+    Spinner type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
-        cardtype = findViewById(R.id.payment_cardtype);
+        type = findViewById(R.id.payment_cardtype);
 
         String[] card ={"VISA","MASTER","AMEX"};
 
-        ArrayAdapter list = new ArrayAdapter(payment.this, android.R.layout.simple_list_item_activated_1,android.R.id.text1,card);
-        cardtype.setAdapter(list);
-        cardtype.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                list.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            }
-        });
+        ArrayAdapter list = new ArrayAdapter(payment.this,R.layout.custom_type_card,android.R.id.text1,card);
+        type.setAdapter(list);
+
     }
 }

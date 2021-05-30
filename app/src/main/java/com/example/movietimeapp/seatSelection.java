9b8e1eab3136b383b21f861a2b7624a03bbbbb23
ckhuey <choongkithuey@gmail.com>
2021-05-30 23:10:47 +0800

    update seat

--- app/src/main/java/com/example/movietimeapp/seatSelection.java
+++ app/src/main/java/com/example/movietimeapp/seatSelection.java
@@ -4,26 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class seatSelection extends AppCompatActivity {
 
-    private ImageButton button;
+    private int i,j;
+    private ImageButton button[][];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat_selection);
 
-        button = findViewById(R.id.bt1);
+        for(int i=0; i<1; i++) {
+            for(int j=0; j<35; j++) {
+                String buttonID = "btn" + i + "-" + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                button[i][j] = ( findViewById(resID));
 
-        button.setOnClickListener(new View.OnClickListener() {
+            }
+        }
+
+        button[i][j].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (button.isSelected()) {
-                    button.setImageDrawable(getResources().getDrawable(R.drawable.selected));
+
+                if (button[i][j].isEnabled()) {
+                    button[i][j].setImageDrawable(getResources().getDrawable(R.drawable.selected));
+
                 }
-                
+                else if (button[i][j].isEnabled()){
+
+                    button[i][j].setImageDrawable(getResources().getDrawable(R.drawable.available));
+                }
             }
         });
     }

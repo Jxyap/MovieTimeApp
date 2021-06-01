783ec349a0c4e5f444efe98bdb15a1fb35299d21
jxyap <82267461+Jxyap@users.noreply.github.com>
2021-06-01 23:59:19 +0800

    bug fixed

--- app/src/main/java/com/example/movietimeapp/Account.java
+++ app/src/main/java/com/example/movietimeapp/Account.java
@@ -6,8 +6,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -15,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;

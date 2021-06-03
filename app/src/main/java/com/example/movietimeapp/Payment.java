b717f9fd67a3b5b7d745fc16b510c9f8a0d4141a
ckhuey <choongkithuey@gmail.com>
2021-06-04 02:19:21 +0800

    update payment

--- app/src/main/java/com/example/movietimeapp/Payment.java
+++ app/src/main/java/com/example/movietimeapp/Payment.java
@@ -2,15 +2,20 @@ package com.example.movietimeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Payment extends AppCompatActivity {
 
-    Spinner type;
+    private Spinner type;
+    private TextView details,payments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +23,25 @@ public class Payment extends AppCompatActivity {
         setContentView(R.layout.activity_payment);
 
         type = findViewById(R.id.payment_cardtype);
+        details = findViewById(R.id.payment_details);
+        payments = findViewById(R.id.payment_price);
 
         String[] card ={"VISA","MASTER","AMEX"};
 
         ArrayAdapter list = new ArrayAdapter(Payment.this,R.layout.custom_type_card,android.R.id.text1,card);
         type.setAdapter(list);
+
+        if (getIntent().hasExtra("cName")){
+        String cinema = getIntent().getStringExtra("cName");
+        String movie = getIntent().getStringExtra("movie");
+        String date = getIntent().getStringExtra("date");
+        String time = getIntent().getStringExtra("time");
+        String payment = getIntent().getStringExtra("price");
+
+         details.setText(movie+" in "+cinema+" at "+time+" ("+date+")");
+         payments.setText("RM"+payment);
+        }
+
 
     }
 }
--- app/src/main/java/com/example/movietimeapp/Ticket_confirmation.java
+++ app/src/main/java/com/example/movietimeapp/Ticket_confirmation.java
@@ -5,25 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.Sampler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Ticket_confirmation extends AppCompatActivity {
 
+    private Button bt_cfm, bt_back;
     private ArrayList<String> seat;
     private TextView seatNo, movieTV, cinemaTV, dateTV, timeTV;
     private String cinema, movie, date, time, back;
     private String seatDisplay ="";
     private String seatChoose[];
+    private String payment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_confirmation);
-        setTitle("confirmation");
+        setTitle("Confirmation");
 
         seatChoose = new String[35];
         Intent intent = getIntent();
@@ -33,6 +37,8 @@ public class Ticket_confirmation extends AppCompatActivity {
         cinemaTV = findViewById(R.id.tv_cinema);
         dateTV = findViewById(R.id.tv_date);
         timeTV = findViewById(R.id.tv_time);
+        bt_cfm = findViewById(R.id.bt_tconfirm);
+        bt_back = findViewById(R.id.bt_tback);
 
         if (intent.hasExtra("cName")) {
             cinema = getIntent().getStringExtra("cName");
@@ -57,6 +63,26 @@ public class Ticket_confirmation extends AppCompatActivity {
             }
             seatNo.setText(seatDisplay);
         }
+
+        int price = 0;
+        for(int i=0;i<seat.size();i++){
+            price = price +10;
+        }
+        payment = (String.valueOf(price));
+        bt_cfm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent confirm2payment= new Intent (Ticket_confirmation.this,Payment.class);
+                if (intent.hasExtra("cName")){
+                    confirm2payment.putExtra("cName", cinema);
+                    confirm2payment.putExtra("movie", movie);
+                    confirm2payment.putExtra("date", date);
+                    confirm2payment.putExtra("time", time);
+                    confirm2payment.putExtra("price", payment);
+                }
+                startActivity(confirm2payment);
+            }
+        });
 
     }
 }

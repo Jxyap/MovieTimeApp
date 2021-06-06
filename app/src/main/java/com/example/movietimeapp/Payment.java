6bcb6f0e6af5cb9089779a69c1e02995ba20b1dc
jxyap <82267461+Jxyap@users.noreply.github.com>
2021-06-06 13:01:54 +0800

    update payment code for seat and save

--- app/src/main/java/com/example/movietimeapp/Payment.java
+++ app/src/main/java/com/example/movietimeapp/Payment.java
@@ -17,77 +17,94 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
+import java.nio.file.FileVisitOption;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Payment extends AppCompatActivity {
 
-    private Spinner type;
-    private TextView details,payments;
     private DatabaseReference databaseReference;
-    private Button confrim_bt;
-    private String cinema, movie,date,time,payment, saveseat;
+    private String cinema;
+    private String movie;
+    private String date;
+    private String time;
+    private String payment;
+    private String saveseat;
     private FirebaseAuth mAuth;
-    private EditText cardnum,et_edate,et_ccv;
+    private EditText cardnum, et_edate, et_ccv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
-        type = findViewById(R.id.payment_cardtype);
-        details = findViewById(R.id.payment_details);
-        payments = findViewById(R.id.payment_price);
-        confrim_bt = findViewById(R.id.payment_confirm);
-        cardnum= findViewById(R.id.card_num);
-        et_edate=findViewById(R.id.et_edate);
+        Spinner type = findViewById(R.id.payment_cardtype);
+        TextView details = findViewById(R.id.payment_details);
+        TextView payments = findViewById(R.id.payment_price);
+        Button confrim_bt = findViewById(R.id.payment_confirm);
+        cardnum = findViewById(R.id.card_num);
+        et_edate = findViewById(R.id.et_edate);
         et_ccv = findViewById(R.id.et_ccv);
 
-        String[] card ={"VISA","MASTER","AMEX"};
+        String[] card = {"VISA", "MASTER", "AMEX"};
 
-        ArrayAdapter list = new ArrayAdapter(Payment.this,R.layout.custom_type_card,android.R.id.text1,card);
+        ArrayAdapter list = new ArrayAdapter(Payment.this, R.layout.custom_type_card, android.R.id.text1, card);
         type.setAdapter(list);
 
-        if (getIntent().hasExtra("cName")){
-            cinema = getIntent().getStringExtra("cName");
-            movie = getIntent().getStringExtra("movie");
-            date = getIntent().getStringExtra("date");
-            time = getIntent().getStringExtra("time");
-            payment = getIntent().getStringExtra("price");
-            saveseat = getIntent().getStringExtra("seat");
-            details.setText(movie+" in "+cinema+" at "+time+" ("+date+")");
-            payments.setText("RM"+payment);
+        cinema = getIntent().getStringExtra("cName");
+        movie = getIntent().getStringExtra("movie");
+        date = getIntent().getStringExtra("date");
+        time = getIntent().getStringExtra("time");
+        payment = getIntent().getStringExtra("price");
+        saveseat = getIntent().getStringExtra("seat");
+        details.setText(movie + " in " + cinema + " at " + time + " (" + date + ")");
+        payments.setText("RM" + payment);
 
-        }
-        mAuth = FirebaseAuth.getInstance();
-
-        confrim_bt.setOnClickListener(new View.OnClickListener() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Seat");
+        databaseReference.child(cinema).child(movie).child(date+" "+time).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-               if (cardnum.getText().toString().length()!=0 && et_edate.getText().toString().length()!=0 && et_ccv.getText().toString().length()!=0 ){
-                    saveSeatTicket();
-                    saveTicketfunction();
-                }
-                else
-                    Toast.makeText(Payment.this, "Please insert all your information", Toast.LENGTH_SHORT).show();
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String seat = snapshot.child("seat").getValue(String.class);
+                saveseat = seat +", "+saveseat;
+
+                mAuth = FirebaseAuth.getInstance();
+                confrim_bt.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (cardnum.getText().toString().length() != 0 && et_edate.getText().toString().length() != 0 && et_ccv.getText().toString().length() != 0) {
+                            saveSeatTicket();
+                            saveTicketfunction();
+                        } else
+                            Toast.makeText(Payment.this, "Please insert all your information", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
-
     }
 
-    public void saveTicketfunction(){
-        final String timestamp = ""+System.currentTimeMillis();
+    public void saveTicketfunction() {
+        final String timestamp = "" + System.currentTimeMillis();
 
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("cinemaName", "" + cinema);
         hashMap.put("movie", "" + movie);
         hashMap.put("date", "" + date);
         hashMap.put("time", "" + time);
-        hashMap.put("seat", "" +saveseat);
-        hashMap.put("ticketID",""+ timestamp);
+        hashMap.put("seat", "" + saveseat);
+        hashMap.put("ticketID", "" + timestamp);
 
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
@@ -96,7 +113,7 @@ public class Payment extends AppCompatActivity {
             public void onSuccess(Void aVoid) {
 
                 Toast.makeText(Payment.this, "Your Payment had successfully", Toast.LENGTH_SHORT).show();
-                Intent intent2Main = new Intent(Payment.this,Homepage.class);
+                Intent intent2Main = new Intent(Payment.this, Homepage.class);
                 startActivity(intent2Main);
                 finish();
 
@@ -109,13 +126,11 @@ public class Payment extends AppCompatActivity {
         });
     }
 
-    public void saveSeatTicket(){
+    public void saveSeatTicket() {
         HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("seat",saveseat);
+        hashMap.put("seat", saveseat);
 
-
-
-        String date_time = date+" "+time;
+        String date_time = date + " " + time;
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Seat");
         databaseReference.child(cinema).child(movie).child(date_time).setValue(hashMap);
 

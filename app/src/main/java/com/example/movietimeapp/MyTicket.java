742e3657da571dcc999258fe5b0bb982308cff38
jxyap <82267461+Jxyap@users.noreply.github.com>
2021-06-06 14:51:28 +0800

    Update MyTicket.java

--- app/src/main/java/com/example/movietimeapp/MyTicket.java
+++ app/src/main/java/com/example/movietimeapp/MyTicket.java
@@ -1,6 +1,7 @@
 package com.example.movietimeapp;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,34 +13,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MyTicket extends ArrayAdapter {
+public class MyTicket extends AppCompatActivity {
 
-    final Context context;
-    private final String movie[];
-    final Integer logo[];
-
-    public MyTicket(@NonNull Context context, String movie[], Integer logo[]) {
-        super(context, R.layout.activity_my_ticket, movie);
-
-        this.context = context;
-        this.movie = movie;
-        this.logo = logo;
-    }
-
-    @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.activity_my_ticket, null, true);
-
-        TextView title = rowView.findViewById(R.id.txt_movie);
-
-        ImageView image = rowView.findViewById(R.id.img_movie);
-
-        title.setText(movie[position]);
-        image.setImageResource(logo[position]);
-
-        return rowView;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ticket);
     }
 }
